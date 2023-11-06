d4855563535a5e6426c22f88add1c48145fbfdb1
dd0minguez <dd0minguez@users.noreply.github.com>
2023-11-06 19:34:42 +0100

    NewClass

---
+++ prueba1/NewClass.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */package programacio;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumne-DAM
+ */
+public class NewClass {
+   public static void main(String[] args) {
+       Scanner consola = new Scanner(System.in);
+
+   //boolean numero = true ;
+    int numeroCompt = 0;
+    while (numeroCompt < 20) {
+        System.out.println(numeroCompt);
+        numeroCompt = numeroCompt + 2;
+    }
+    System.out.println("S'ha sumat 1 i ara es: " + numeroCompt);
+    
+       System.out.print("Escriu una frase: ");
+    String fraseUsuaio = "Hola mundo";
+    System.out.println("Has escrit: " + fraseUsuaio);
+    }
+}
